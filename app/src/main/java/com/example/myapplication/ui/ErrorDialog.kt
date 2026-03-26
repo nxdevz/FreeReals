6b7ce70d5b9f49fc2b3e40f98d6e0076f1f3f6b2
nxdevz <mkang3753@gmail.com>
2026-03-27 06:25:53 +0700

    Update ErrorDialog.kt

--- app/src/main/java/com/example/myapplication/ui/ErrorDialog.kt
+++ app/src/main/java/com/example/myapplication/ui/ErrorDialog.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.utils.ErrorLogger
 
 @Composable
